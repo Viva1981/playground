325ccac1d321ea5890fc97c6a89c60c83e77c5b9
Viva1981 <stuller.zsolt@gmail.com>
2026-01-27 19:11:04 +0100

    reset project to clean base

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,13 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css"; // Ez a sor tölti be a dizájnt!
-
-const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "UI Design Playground",
-  description: "Interaktív React Design Rendszer",
-};
+import "./globals.css";
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="hu">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
